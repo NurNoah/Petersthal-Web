eb257dd89f3bc20a09898c9e103f5b7e48b39348
Noah Weißenbach <weissenbachnoah@gmail.com>
2025-09-17 22:14:39 +0000

    Sehr gut mache nun ganz unten noch 2 kleine google maps einbetugnen von

--- src/app/anfahrt/page.tsx
+++ src/app/anfahrt/page.tsx
@@ -17,7 +17,7 @@ export default function AnfahrtPage() {
         <h2 className="text-2xl font-bold font-headline mb-4">Karte</h2>
         <div className="flex justify-center">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2688.5173735191183!2d10.383165876701055!3d47.635512135980065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479c7d111d1678b7%3A0xa1e48b04cf72010!2s87466%20Oy-Mittelberg-Petersthal!5e0!3m2!1sde!2sde!4v1758146173184!5m2!1sde!2sde"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2688.5173735191183!2d10.383165876701055!3d47.635512135980065!2m3!1f0!2f0!3f0!3m2!i1024!2i768!4f13.1!3m3!1m2!1s0x479c7d111d1678b7%3A0xa1e48b04cf72010!2s87466%20Oy-Mittelberg-Petersthal!5e0!3m2!1sde!2sde!4v1758146173184!5m2!1sde!2sde"
             width="1400"
             height="400"
             style={{ border: 0 }}
@@ -79,6 +79,36 @@ export default function AnfahrtPage() {
             </AccordionItem>
           ))}
         </Accordion>
+      </section>
+
+      <section className="mt-12">
+        <h2 className="text-2xl font-bold font-headline mb-4">Bushaltestellen</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div>
+            <h3 className="text-lg font-semibold mb-2">Haltestelle Petersthal Kirche</h3>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2688.468898135893!2d10.38421457670107!3d47.63645063598501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479c7d1010190473%3A0xf601b559773730e7!2sPetersthal%20Kirche%2C%2087466%20Oy-Mittelberg!5e0!3m2!1sde!2sde!4v1758153495209!5m2!1sde!2sde"
+              width="100%"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold mb-2">Haltestelle Petersthal Badeseeweg</h3>
+             <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2688.583568461011!2d10.37517117670098!3d47.6340332360012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479c7d1746241b39%3A0xe16b801a2f646039!2sPetersthal%20Badeseeweg%2C%2087466%20Oy-Mittelberg!5e0!3m2!1sde!2sde!4v1758153549929!5m2!1sde!2sde"
+              width="100%"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
       </section>
     </div>
   );
